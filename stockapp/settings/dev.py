66c92621a1fcd579373e6e51e3199ab2c510b9f1
JeromeChu <homechu58@gmail.com>
2024-08-22 11:13:26 +0800

    init

---
+++ stockapp/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-awa$=^&^494a-&@v+i%36d-x8b&l2$updml+a6-dv5)420b0se"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
